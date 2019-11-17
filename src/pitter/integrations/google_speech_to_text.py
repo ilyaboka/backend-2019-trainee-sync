3c34c71f1b02b0a5bc2d30e24987cd1080ac676c
Ilya Boka <bokailya@gmail.com>
2019-11-17 22:33:09 +0200

    black reformating

--- src/pitter/integrations/google_speech_to_text.py
+++ src/pitter/integrations/google_speech_to_text.py
@@ -7,6 +7,7 @@ import magic
 
 from src.pitter.exceptions import ValidationError
 
+
 class GoogleSpeechToText:
     URL: str = 'https://speech.googleapis.com/v1/speech:recognize'
     GOOGLE_API_KEY: str = environ['GOOGLE_API_KEY']
@@ -20,10 +21,10 @@ class GoogleSpeechToText:
         if mime_type not in ['audio/flac', 'audio/x-wav']:
             raise ValidationError(message='Invalid speech file', status_code=415)
 
-
         return loads(
             post(
-                cls.URL, params=dict(key=cls.GOOGLE_API_KEY,),
-                json=dict(audio=dict(content=b64encode(audio_file).decode(),), config=config,)
+                cls.URL,
+                params=dict(key=cls.GOOGLE_API_KEY,),
+                json=dict(audio=dict(content=b64encode(audio_file).decode(),), config=config,),
             ).text
         )['results'][0]['alternatives'][0]['transcript']
