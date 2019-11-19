ac2daf89b23496dfd929bd8aff2a1ab76965b139
Ilya Boka <bokailya@gmail.com>
2019-11-19 16:54:59 +0200

    Ticket view error fixed

--- src/api_client/views/ticket.py
+++ src/api_client/views/ticket.py
@@ -39,6 +39,12 @@ class TicketMobileView(APIView):
         user_comment: str = request.data['userComment']
         fake_id: str = str(uuid4())
 
-        result = Ticket.create_ticket(fake_id=fake_id, message=message, user_comment=user_comment,)
+        result: Ticket = Ticket.create_ticket(fake_id=fake_id, message=message, user_comment=user_comment,)
+        result_dict: dict = result.to_dict()
 
-        return result.to_dict()
+        return dict(
+            id=result_dict["id"],
+            fakeId=result_dict["fake_id"],
+            message=result_dict["message"],
+            userComment=result_dict["user_comment"],
+        )
