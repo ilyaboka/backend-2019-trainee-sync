5e3cc1d6f81836587dc659f6e304496bd09e0f51
Ilya Boka <bokailya@gmail.com>
2019-11-26 21:10:48 +0200

    Send audiofile inside form

--- src/pitter/utils/recognize.py
+++ src/pitter/utils/recognize.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+import magic
 from requests import post
 from requests.exceptions import RequestException
 
@@ -10,7 +11,10 @@ from pitter.settings import ASYNCHRONOUS_SERVICE_URL
 def recognize(speech_file: bytes) -> str:
     """Вернуть распознанный текст из аудиофайла с речью"""
     try:
-        recognized_rext: str = post(ASYNCHRONOUS_SERVICE_URL, data=speech_file).json()['recognizedText']
+        recognized_rext: str = post(
+            ASYNCHRONOUS_SERVICE_URL,
+            files=dict(speechFile=('speechFile', speech_file, magic.from_buffer(speech_file, mime=True,))),
+        ).json()['recognizedText']
     except (RequestException, KeyError, ValueError) as exception:
         raise exceptions.PitterException("Something went wrong", HTTPStatus.INTERNAL_SERVER_ERROR.value) from exception
     return recognized_rext
