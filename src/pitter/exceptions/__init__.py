95e07b234e83b1dd88dd2a947b33d80089991de9
Ilya Boka <bokailya@gmail.com>
2019-12-06 15:25:54 +0200

    Import order fixed

--- src/pitter/exceptions/__init__.py
+++ src/pitter/exceptions/__init__.py
@@ -1,12 +1,12 @@
 from typing import Sequence
 
-from .exceptions import UnauthorizedError
 from .exceptions import BadRequestError
 from .exceptions import ConflictError
 from .exceptions import ExceptionResponse
 from .exceptions import InternalServerError
 from .exceptions import NotFoundError
 from .exceptions import PitterException
+from .exceptions import UnauthorizedError
 from .exceptions import UnsupportedMediaTypeError
 from .exceptions import ValidationError
 
