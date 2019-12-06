ceb84511a921cc52175e2685bb29a270fa0c030e
Ilya Boka <bokailya@gmail.com>
2019-12-06 06:46:27 +0200

    response_list_serializer removed

--- src/pitter/decorators/__init__.py
+++ src/pitter/decorators/__init__.py
@@ -4,12 +4,10 @@ from .authorization import access_token_required
 from .view_validation import request_body_serializer
 from .view_validation import request_query_parameters_serializer
 from .view_validation import response_dict_serializer
-from .view_validation import response_list_serializer
 
 __all__: Sequence[str] = [
     'access_token_required',
     'request_body_serializer',
     'request_query_parameters_serializer',
     'response_dict_serializer',
-    'response_list_serializer',
 ]
