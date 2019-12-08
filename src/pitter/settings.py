d9835ff62571131ae266c71c02e8774bd4f2062b
Ilya Boka <bokailya@gmail.com>
2019-12-08 11:23:43 +0200

    Lines in settings sorted

--- src/pitter/settings.py
+++ src/pitter/settings.py
@@ -5,12 +5,37 @@ from typing import List
 from typing import Tuple
 from typing import Union
 
+ALLOWED_HOSTS: List[str] = ['*']  # On develop only
+
+ASYNCHRONOUS_SERVICE_URL: str = 'http://pitter_async:8118/api/pitter/v1/recognize'
+
+AUTH_PASSWORD_VALIDATORS: List[Dict[str, str]] = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
 BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY: str = 'cru)q9q-!=#ip!)(i=rawgbjdfxiyrm+znk05iz=5p*w7r9(yh'
+
+DATABASES: Dict[str, Dict[str, str]] = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE', 'postgres'),
+        'USER': os.getenv('PGUSER', 'postgres'),
+        'PASSWORD': os.getenv('PGPASSWORD', 'postgres'),
+        'HOST': os.getenv('PGHOST', 'localhost'),
+        'PORT': os.getenv('PGPORT', '5432'),
+    }
+}
 
 DEBUG: bool = bool(int(os.getenv('DEBUG', 1)))  # pylint: disable=invalid-envvar-default
 
-ALLOWED_HOSTS: List[str] = ['*']  # On develop only
+EMAIL_HOST: str = os.getenv('EMAIL_HOST', 'localhost')
+EMAIL_PORT: int = int(os.getenv('EMAIL_PORT', '25'))
+EMAIL_HOST_USER: str = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD: str = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_SSL: bool = bool(os.getenv('EMAIL_USE_SSL', 'False'))
 
 INSTALLED_APPS: List[str] = [
     'django.contrib.admin',
@@ -25,6 +50,12 @@ INSTALLED_APPS: List[str] = [
     'api_client',
 ]
 
+JSON_WEB_TOKEN_LIFETIME: timedelta = timedelta(hours=1)
+JSON_WEB_TOKEN_PRIVATE_KEY: bytes = open('token_keys/id_rsa', 'rb').read()
+JSON_WEB_TOKEN_PUBLIC_KEY: bytes = open('token_keys/id_rsa.pub', 'rb').read()
+
+LANGUAGE_CODE: str = 'en-us'
+
 MIDDLEWARE: List[str] = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -38,6 +69,18 @@ MIDDLEWARE: List[str] = [
 ]
 
 ROOT_URLCONF: str = 'pitter.urls'
+
+SECRET_KEY: str = 'cru)q9q-!=#ip!)(i=rawgbjdfxiyrm+znk05iz=5p*w7r9(yh'
+
+SECURE_PROXY_SSL_HEADER: Tuple[str, str] = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATICFILES_DIRS: List[str] = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_URL: str = '/static/'
+
+STATIC_ROOT: str = '/static'
 
 TEMPLATES: List[Dict[str, Union[Dict[str, List[str]], List, bool, str]]] = [
     {
@@ -55,31 +98,9 @@ TEMPLATES: List[Dict[str, Union[Dict[str, List[str]], List, bool, str]]] = [
     },
 ]
 
-DATABASES: Dict[str, Dict[str, str]] = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PGDATABASE', 'postgres'),
-        'USER': os.getenv('PGUSER', 'postgres'),
-        'PASSWORD': os.getenv('PGPASSWORD', 'postgres'),
-        'HOST': os.getenv('PGHOST', 'localhost'),
-        'PORT': os.getenv('PGPORT', '5432'),
-    }
-}
-
-WSGI_APPLICATION: str = 'pitter.wsgi.application'
-USE_X_FORWARDED_HOST: bool = True
-SECURE_PROXY_SSL_HEADER: Tuple[str, str] = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-AUTH_PASSWORD_VALIDATORS: List[Dict[str, str]] = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
-
-LANGUAGE_CODE: str = 'en-us'
-
 TIME_ZONE: str = 'Europe/Moscow'
+
+USE_X_FORWARDED_HOST: bool = True
 
 USE_I18N: bool = True
 
@@ -87,13 +108,7 @@ USE_L10N: bool = True
 
 USE_TZ: bool = True
 
-STATICFILES_DIRS: List[str] = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_URL: str = '/static/'
-
-STATIC_ROOT: str = '/static'
+WSGI_APPLICATION: str = 'pitter.wsgi.application'
 
 # DRF
 
@@ -111,17 +126,3 @@ SWAGGER_SETTINGS: Dict[str, Union[Dict[str, Dict[str, str]], bool]] = {
         'X-Device-Info': {'type': 'apiKey', 'name': 'X-Device-Info', 'in': 'header'},
     },
 }
-
-ASYNCHRONOUS_SERVICE_URL: str = 'http://pitter_async:8118/api/pitter/v1/recognize'
-
-EMAIL_HOST: str = os.getenv('EMAIL_HOST', 'localhost')
-EMAIL_PORT: int = int(os.getenv('EMAIL_PORT', '25'))
-EMAIL_HOST_USER: str = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD: str = os.getenv('EMAIL_HOST_PASSWORD', '')
-EMAIL_USE_SSL: bool = bool(os.getenv('EMAIL_USE_SSL', 'False'))
-
-JSON_WEB_TOKEN_LIFETIME: timedelta = timedelta(hours=1)
-
-JSON_WEB_TOKEN_PRIVATE_KEY: bytes = open('token_keys/id_rsa', 'rb').read()
-
-JSON_WEB_TOKEN_PUBLIC_KEY: bytes = open('token_keys/id_rsa.pub', 'rb').read()
